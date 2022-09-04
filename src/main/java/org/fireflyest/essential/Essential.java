985ac98550c0a9cf628e786adfebe11f4062e11a
Fireflyest <fireflyest@qq.com>
2022-09-04 19:30:58 +0800

    initial

---
+++ src/main/java/org/fireflyest/essential/Essential.java
@@ -0,0 +1,18 @@
+package org.fireflyest.essential;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Essential extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+
+}
