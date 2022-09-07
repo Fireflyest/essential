765997d0fc7dda75ff42e6f38f60320656964917
Fireflyest <fireflyest@qq.com>
2022-09-07 19:58:13 +0800

    添加Readme

--- src/main/java/org/fireflyest/essential/Essential.java
+++ src/main/java/org/fireflyest/essential/Essential.java
@@ -2,16 +2,27 @@ package org.fireflyest.essential;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * essential
+ */
 public class Essential extends JavaPlugin {
-    
+
+    private static JavaPlugin plugin;
+
     @Override
     public void onEnable() {
+        plugin = this;
 
+        plugin.getLogger().info("");
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static JavaPlugin getPlugin() {
+        return plugin;
     }
 
 
